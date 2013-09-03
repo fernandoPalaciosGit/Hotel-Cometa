8ceadf145b07be67d18663b195a42b71c2134c8e
fernando palacios <fernandopalacioslandi@gmail.com>
2013-09-03 17:24:18 +0200

    navegacion sliders por enlaces

--- js/main.js
+++ js/main.js
@@ -7,8 +7,33 @@ var init = function(){
 	}else if(window.innerWidth > 480){
 		$("#navHotel").fadeOut();
 	}
+	/*EVENTOS MOUSE*/
 	$("#encabezado").on("mousemove", rolloverHeader);
 	$("#navHotel a").on("click", navegacion);
+	$("#control-sliders ul li").on("click", pasarSlides);
+	/*ESPECIFICAR LA DIMENSIONES DE CADA SLIDE*/
+	var widthParent = $("#contenedor-sliders").width();
+	$(".slider").each(function(index, item){
+		var uri = $(item).data("background");
+		$(item).css("width", widthParent+"px");
+		$(item).css("background-image", "url('"+uri+"')");
+	});
+
+}
+var pasarSlides = function(){
+	var slideTarget = 0;
+	/*¿se paso por un intervalo (TIEMPO) o por un boton (CLICK)?*/
+	if($(this).closest("#control-sliders").hasClass("pasar-slide")){
+		slideTarget = $(this).index();/*devuelve el indice del array*/
+
+	}
+	$("#img-sliders").fadeOut("slow", function(){
+		$(this).animate({
+			"margin-left" : -(slideTarget * $(this).parent().width())+"px"
+		}, "slow", function(){
+			$(this).fadeIn();
+		});
+	});
 }
 var rolloverHeader = function(event){
 	if(window.innerWidth <= 480){
