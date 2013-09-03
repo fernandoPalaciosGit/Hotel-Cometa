fb8038558583e32bf5cc49bf1ca91beb29326efe
fernando palacios <fernandopalacioslandi@gmail.com>
2013-09-03 15:17:01 +0200

    optimizar codigo de rollover en navegacion

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-$("#encabezado").on("mousemove", function(event){
+$(document).ready(function(){
+	init();
+});
+var init = function(){
+	if(window.innerWidth <= 480){
+		$("#navHotel").fadeIn();
+	}else if(window.innerWidth > 480){
+		$("#navHotel").fadeOut();
+	}
+	$("#encabezado").on("mousemove", rolloverHeader);
+	$("#navHotel a").on("click", navegacion);
+}
+var rolloverHeader = function(event){
+	if(window.innerWidth <= 480){
+		return;
+	}
 	if((event.pageX<590 && event.pageX>410 && event.pageY>100) || (event.pageX>590 && event.pageY>50) || (event.pageX<410 && event.pageY>50)){
 		$("#navHotel a").trigger("click");
 	}else{
@@ -6,9 +21,9 @@ $("#encabezado").on("mousemove", function(event){
 		$("#navHotel").fadeIn();
 		$("#eslogan").fadeOut();
 	}
-});
-$("#navHotel a").on("click", function(){
+}
+var navegacion = function(){
 	$("#encabezado").css({"margin-top" : "-10rem"});
 	$("#navHotel").fadeOut();
 	$("#eslogan").fadeIn();
-});
+}
